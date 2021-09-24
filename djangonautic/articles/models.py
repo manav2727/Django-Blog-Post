f0213a98a63350aac22c412cada7dae9d907357f
manav2727 <manavmajithia6@gmail.com>
2021-09-24 22:53:49 +0530

    first commit

---
+++ djangonautic/articles/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models.fields import DateField
+from django.urls.converters import SlugConverter
+from django.contrib.auth.models import User
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True) # whenever the post is created, date is auto generated and stored 
+    thumb = models.ImageField(default='default.png', blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:50] + "..."
